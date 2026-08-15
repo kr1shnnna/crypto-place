c9f4e8a223bae72a670b267194fecd4e2800b68d
Krishna <krishna9842740994@gmail.com>
2026-08-15 23:58:50 +0530

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>
+            Copyright © 2026 CryptoPlace. All rights reserved.
+        </p>
+    </div>
+  )
+}
+
+export default Footer
