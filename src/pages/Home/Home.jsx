6b14df0bde0a1f1bbfb6bbd075b71970858d7229
Krishna <krishna9842740994@gmail.com>
2026-08-16 00:20:52 +0530

    added input options

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,85 +1,100 @@
-import './Home.css'
-import { useContext } from 'react'
-import { CoinContext } from '../../context/CoinContext.jsx'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import "./Home.css";
+import { useContext } from "react";
+import { CoinContext } from "../../context/CoinContext.jsx";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Home = () => {
+  const { allCoin, currency } = useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+  const [input, setInput] = useState("");
 
+  const inputHandler = (e) => {
+    setInput(e.target.value);
 
-    
-    const {allCoin,currency}=useContext(CoinContext);
-    const [displayCoin,setDisplayCoin]=useState([]);
-    const [input,setInput]=useState('');
-
-    const inputHandler=(e)=>{
-
-        setInput(e.target.value);
-
+    if (e.target.value === "") {
+      setDisplayCoin(allCoin);
     }
+  };
 
-    const searchHandler=async(e)=>{
-        e.preventDefault();
+  const searchHandler = async (e) => {
+    e.preventDefault();
 
-       const coins= await allCoin.filter((item)=>{
+    const coins = await allCoin.filter((item) => {
+      return item.name.toLowerCase().includes(input.toLowerCase());
+    });
 
-          return  item.name.toLowerCase().includes(input.toLowerCase())
+    setDisplayCoin(coins);
+  };
 
-        })
-
-        setDisplayCoin(coins);
-
-    }
-
-    useEffect(()=>{
-
-        setDisplayCoin(allCoin);
-    },[allCoin]);
+  useEffect(() => {
+    setDisplayCoin(allCoin);
+  }, [allCoin]);
   return (
-    <div className='home'>
-        <div className="hero">
-            <h1>Largest <br/>Crypto Marketplace</h1>
-            <p>Welcome to the world's largest cryptocurrency marketplace. Sign up to explore more about cryptos.</p>
+    <div className="home">
+      <div className="hero">
+        <h1>
+          Largest <br />
+          Crypto Marketplace
+        </h1>
+        <p>
+          Welcome to the world's largest cryptocurrency marketplace. Sign up to
+          explore more about cryptos.
+        </p>
 
+        <form onSubmit={searchHandler}>
+          <input
+            list='coinlist'
+            onChange={inputHandler}
+            value={input}
+            type="text"
+            placeholder="Search crypto .."
+            required
+          />
 
-            <form  onSubmit={searchHandler}>
+          <datalist id='coinlist'>
+            {allCoin.map((item,index)=>(<option key={index} value={item.name}/>)
 
+            )}
 
-                <input onChange={inputHandler} value={input}
-                type='text' placeholder='Search crypto ..' required/>
-                <button type='submit'>Search</button>
-            </form>
+          </datalist>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p style={{ textAlign: "center" }}>24H Change</p>
+          <p className="market-cap">Market Cap</p>
         </div>
-
-        <div className="crypto-table">
-            <div className="table-layout">
-                <p>#</p>
-                <p>Coins</p>
-                <p>Price</p>
-                <p style={{textAlign:'center'}}>24H Change</p>
-                <p className='market-cap'>Market Cap</p>
-
+        {displayCoin.slice(0, 10).map((item, index) => (
+          <div className="table-layout" key={index}>
+            <p>{item.market_cap_rank}</p>
+            <div>
+              <img src={item.image} />
+              <p>{item.name + "-" + item.symbol}</p>
             </div>
-            {
-                displayCoin.slice(0,10).map((item,index)=>(
-                    <div className="table-layout" key={index}>
-                        <p>{item.market_cap_rank}</p>
-                        <div>
-                            <img  src={item.image}/>
-                            <p>{item.name +'-'+item.symbol}</p>
-                        </div>
-                        <p>{currency.symbol}{item.current_price.toLocaleString()}</p>
-                        <p className={item.price_change_percentage_24h>0?'green':'red'}>
-                            {Math.floor(item.price_change_percentage_24h*100)/100}</p>
-                        <p className='market-cap'>{currency.symbol}{item.market_cap.toLocaleString()}</p>
-                    </div>
-                ))
-            }
-        </div>
-
-       
+            <p>
+              {currency.symbol}
+              {item.current_price.toLocaleString()}
+            </p>
+            <p
+              className={item.price_change_percentage_24h > 0 ? "green" : "red"}
+            >
+              {Math.floor(item.price_change_percentage_24h * 100) / 100}
+            </p>
+            <p className="market-cap">
+              {currency.symbol}
+              {item.market_cap.toLocaleString()}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
